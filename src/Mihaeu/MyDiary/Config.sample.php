b88b2c0feb0b64db01a9440b50d338f824a6ee2a
Michael Haeuslmann <haeuslmann@gmail.com>
2013-12-14 21:11:11 +0545

    Added Easybook and bootstrapping for the diary.

---
+++ src/Mihaeu/MyDiary/Config.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mihaeu\MyDiary;
+
+class Config
+{
+    public $slug = 'example';
+    public $title = 'Example';
+    public $author = 'Mr. Example';
+    public $pathToEntries = '/example/path/to/entries';
+}
